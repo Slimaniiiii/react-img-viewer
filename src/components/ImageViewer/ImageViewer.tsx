58ab14575d4b30e6bb83d3459e252f21c0785fb1
Houssem Slimani <houssem.slimeni@avaxia-group.com>
2022-09-20 16:39:03 +0100

    img move

--- src/components/ImageViewer/ImageViewer.tsx
+++ src/components/ImageViewer/ImageViewer.tsx
@@ -43,35 +43,60 @@ const ImageViewer = (props: imageProps) => {
     setImgSize();
   };
 
+  let gMouseDownX = 0;
+  let gMouseDownY = 0;
+  let gMouseDownOffsetX = 0;
+  let gMouseDownOffsetY = 0;
+
   function addListeners() {
-    let haja: any = document.getElementById("testimg");
-    if (haja) {
-      haja.addEventListener("mousedown", mouseDown, false);
-      window.addEventListener("mouseup", mouseUp, false);
-    }
+    let doc: any = document.getElementById("testimg");
+    doc.addEventListener("mousedown", mouseDown, false);
+    window.addEventListener("mouseup", mouseUp, false);
   }
 
   function mouseUp() {
     window.removeEventListener("mousemove", divMove, true);
   }
 
-  function mouseDown() {
+  function mouseDown(e: any) {
+    gMouseDownX = e.clientX;
+    gMouseDownY = e.clientY;
+
+    let div: any = document.getElementById("testimg");
+
+    //The following block gets the X offset (the difference between where it starts and where it was clicked)
+    let leftPart = "";
+    if (!div.style.left)
+      leftPart += "0px"; //In case this was not defined as 0px explicitly.
+    else leftPart = div.style.left;
+    let leftPos = leftPart.indexOf("px");
+    let leftNumString = leftPart.slice(0, leftPos); // Get the X value of the object.
+    gMouseDownOffsetX = gMouseDownX - parseInt(leftNumString, 10);
+
+    //The following block gets the Y offset (the difference between where it starts and where it was clicked)
+    let topPart = "";
+    if (!div.style.top)
+      topPart += "0px"; //In case this was not defined as 0px explicitly.
+    else topPart = div.style.top;
+    let topPos = topPart.indexOf("px");
+    let topNumString = topPart.slice(0, topPos); // Get the Y value of the object.
+    gMouseDownOffsetY = gMouseDownY - parseInt(topNumString, 10);
+
     window.addEventListener("mousemove", divMove, true);
   }
-
   function divMove(e: any) {
-    const div = document.getElementById("testimg");
+    let div = document.getElementById("testimg");
     if (div) {
       div.style.position = "absolute";
-      div.style.top = e.clientY + "px";
-      div.style.left = e.clientX + "px";
+      let topAmount = e.clientY - gMouseDownOffsetY;
+      div.style.top = topAmount + "px";
+      let leftAmount = e.clientX - gMouseDownOffsetX;
+      div.style.left = leftAmount + "px";
     }
   }
+
   useEffect(() => {
     addListeners();
-    return () => {
-      window.removeEventListener("mousemove", divMove, true);
-    };
   }, []);
 
   return (
