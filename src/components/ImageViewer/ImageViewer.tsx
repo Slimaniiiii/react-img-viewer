54bf60bcaca2a9a7aa91917f1713b2a5b679db2a
Houssem Slimani <houssem.slimeni@avaxia-group.com>
2022-09-20 11:27:25 +0100

    set image sizing

--- src/components/ImageViewer/ImageViewer.tsx
+++ src/components/ImageViewer/ImageViewer.tsx
@@ -14,18 +14,48 @@ interface imageProps {
 const ImageViewer = (props: imageProps) => {
   const [model, setModel] = useState(false);
   const [tempImgSrc, setTempImgSrc] = useState("");
-  const imgRef = useRef(null);
+  const imgRef: any = useRef(null);
 
+  //get image size
+  const imgSize = () => {
+    if (imgRef.current) {
+      const height = imgRef.current.clientHeight;
+      const width = imgRef.current.clientWidth;
+      return { height, width };
+    }
+  };
+  const setImgSize = () => {
+    const { height, width }: any = imgSize();
+    const modelImg: any = document.querySelector(".model img");
+    modelImg.style.height = `${height}px`;
+    modelImg.style.width = `${width}px`;
+  };
   const getImg = (img: string) => {
     setTempImgSrc(img);
     setModel(true);
+    setImgSize();
   };
+
+
+  // .model img {
+  //   width: auto;
+  //   max-width: 100%;
+  //   height: auto;
+  //   max-height: 100%;
+  //access to .model img and set height and width
+
+
 
   return (
     <>
       <div className={model ? "model open" : "model"}>
         <img ref={imgRef} src={tempImgSrc} alt="image" />
-        <CloseIcon id="closesvg" width={30} height={30} onClick={() => setModel(false)} />
+        <CloseIcon
+          id="closesvg"
+          width={30}
+          height={30}
+          onClick={() => setModel(false)}
+        />
         <div className="configIcons">
           <ArrowLeft width={25} height={25} />
           <ArrowRight width={25} height={25} />
