2aa1a5ad0b326944d03247c54eb73bfeb8b3c654
Houssem Slimani <houssem.slimeni@avaxia-group.com>
2022-09-21 11:04:10 +0100

    eeaze

--- src/components/ImageViewer/ImageViewer.tsx
+++ src/components/ImageViewer/ImageViewer.tsx
@@ -44,64 +44,64 @@ const ImageViewer = (props: imageProps) => {
   };
 
   let gMouseDownX = 0;
-  let gMouseDownY = 0;
-  let gMouseDownOffsetX = 0;
-  let gMouseDownOffsetY = 0;
+let gMouseDownY = 0;
+let gMouseDownOffsetX = 0;
+let gMouseDownOffsetY = 0;
+
+function addListeners() {
+  let doc: any = document.getElementById("testimg");
+  doc.addEventListener("mousedown", mouseDown, false);
+  window.addEventListener("mouseup", mouseUp, false);
+}
+
+function mouseUp() {
+  window.removeEventListener("mousemove", divMove, true);
+}
+
+function mouseDown(e: any) {
+  gMouseDownX = e.clientX;
+  gMouseDownY = e.clientY;
+
+  let div: any = document.getElementById("testimg");
+
+  //The following block gets the X offset (the difference between where it starts and where it was clicked)
+  let leftPart = "";
+  if (!div.style.left)
+    leftPart += "0px"; //In case this was not defined as 0px explicitly.
+  else leftPart = div.style.left;
+  let leftPos = leftPart.indexOf("px");
+  let leftNumString = leftPart.slice(0, leftPos); // Get the X value of the object.
+  gMouseDownOffsetX = gMouseDownX - parseInt(leftNumString, 10);
+
+  //The following block gets the Y offset (the difference between where it starts and where it was clicked)
+  let topPart = "";
+  if (!div.style.top)
+    topPart += "0px"; //In case this was not defined as 0px explicitly.
+  else topPart = div.style.top;
+  let topPos = topPart.indexOf("px");
+  let topNumString = topPart.slice(0, topPos); // Get the Y value of the object.
+  gMouseDownOffsetY = gMouseDownY - parseInt(topNumString, 10);
   
-  function addListeners() {
-    let doc: any = document.getElementById("testimg");
-    doc.addEventListener("mousedown", mouseDown, false);
-    window.addEventListener("mouseup", mouseUp, false);
+
+  window.addEventListener("mousemove", divMove, true);
+}
+function divMove(e: any) {
+  let div = document.getElementById("testimg");
+  if (div) {
+    div.style.position = "absolute";
+    let topAmount = e.clientY - gMouseDownOffsetY;
+    div.style.top = topAmount + "px";
+    let leftAmount = e.clientX - gMouseDownOffsetX;
+    div.style.left = leftAmount + "px";
   }
-  
-  function mouseUp() {
+}
+
+useEffect(() => {
+  addListeners();
+  return () => {
     window.removeEventListener("mousemove", divMove, true);
   }
-  
-  function mouseDown(e: any) {
-    gMouseDownX = e.clientX;
-    gMouseDownY = e.clientY;
-  
-    let div: any = document.getElementById("testimg");
-  
-    //The following block gets the X offset (the difference between where it starts and where it was clicked)
-    let leftPart = "";
-    if (!div.style.left)
-      leftPart += "0px"; //In case this was not defined as 0px explicitly.
-    else leftPart = div.style.left;
-    let leftPos = leftPart.indexOf("px");
-    let leftNumString = leftPart.slice(0, leftPos); // Get the X value of the object.
-    gMouseDownOffsetX = gMouseDownX - parseInt(leftNumString, 10);
-  
-    //The following block gets the Y offset (the difference between where it starts and where it was clicked)
-    let topPart = "";
-    if (!div.style.top)
-      topPart += "0px"; //In case this was not defined as 0px explicitly.
-    else topPart = div.style.top;
-    let topPos = topPart.indexOf("px");
-    let topNumString = topPart.slice(0, topPos); // Get the Y value of the object.
-    gMouseDownOffsetY = gMouseDownY - parseInt(topNumString, 10);
-    
-  
-    window.addEventListener("mousemove", divMove, true);
-  }
-  function divMove(e: any) {
-    let div = document.getElementById("testimg");
-    if (div) {
-      div.style.position = "absolute";
-      let topAmount = e.clientY ;
-      div.style.top = topAmount + "px";
-      let leftAmount = e.clientX;
-      div.style.left = leftAmount + "px";
-    }
-  }
-
-  useEffect(() => {
-    addListeners();
-    return () => {
-      window.removeEventListener("mousemove", divMove, true);
-    }
-  }, []);
+}, []);
 
   return (
     <>
